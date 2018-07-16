5c934e9a0bf03ee31be99e64e7966bb176a0253f
pvraju1988 <venkatrajuj2ee@gmail.com>
2018-07-16 23:40:23 +0530

    new branch

--- GitExample/src/com/venkat/GitCommands.java
+++ GitExample/src/com/venkat/GitCommands.java
@@ -7,6 +7,7 @@ public class GitCommands {
 		System.out.println("Hello Git");
 		System.out.println("Hello2");
 		System.out.println("Hello3");
+		System.out.println("HelloJava");
 		
 		 if(i==0)
 		 {
