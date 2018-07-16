542c4f816f3510eb72ccca4ed888aebed7a8518b
pvraju1988 <venkatrajuj2ee@gmail.com>
2018-07-16 23:16:31 +0530

    Revert "added hello3"
    This reverts commit 8897c5dc5d834c1761f64cc0538b2bf9432cd949.

--- GitExample/src/com/venkat/GitCommands.java
+++ GitExample/src/com/venkat/GitCommands.java
@@ -7,7 +7,6 @@ public class GitCommands {
 		System.out.println("Hello Git");
 		System.out.println("Hello2");
 		System.out.println("Hello3");
-		//System.out.println("Hello4");
 		
 		 if(i==0)
 		 {
